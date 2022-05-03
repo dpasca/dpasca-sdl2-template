b61ecd978a285e755e45b5fe789adf2c15da87e3
Davide Pasca <davide@newtypekk.com>
2022-05-04 01:07:39 +0900

    First commit

---
+++ Apps/Demo1/src/main.cpp
@@ -0,0 +1,15 @@
+//==================================================================
+/// main.cpp
+///
+/// Created by Davide Pasca - 2022/05/04
+/// See the file "license.txt" that comes with this project for
+/// copyright info.
+//==================================================================
+
+#include <stdio.h>
+
+int main( int argc, const char *argv[] )
+{
+    printf( "Simplicity.\n" );
+    return 0;
+}
