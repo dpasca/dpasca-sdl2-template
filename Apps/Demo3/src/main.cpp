bf98d482e10d3b7cd1a9814f643fb30d490f46c8
Davide Pasca <davide@newtypekk.com>
2022-05-04 19:53:51 +0900

    [DEMO3] First version. 3D demo using glm

---
+++ Apps/Demo3/src/main.cpp
@@ -0,0 +1,277 @@
+//==================================================================
+/// main.cpp
+///
+/// Created by Davide Pasca - 2022/05/04
+/// See the file "license.txt" that comes with this project for
+/// copyright info.
+//==================================================================
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <array>
+#include <vector>
+#include <algorithm> // for std::sort
+
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/ext/matrix_transform.hpp>  // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/scalar_constants.hpp>  // glm::pi
+
+#include "MinimalSDLApp.h"
+
+using V3F = glm::vec3;
+using V3I = glm::ivec3;
+
+using MAT4 = glm::mat4;
+
+// random between 0 and 1
+inline float randUnit()
+{
+    return (float)(rand() % 10000) / (10000.f-1);
+};
+
+// just a lerp
+inline auto dlerp = []( const auto &l, const auto &r, const auto t )
+{
+    return l * (1 - t) + r * t;
+};
+
+//==================================================================
+static std::array<V3F,8> makeCubeVerts( const V3F &mi, const V3F &ma )
+{
+/*
+        y          2 ----- 6           __________
+        |__ x    / |     / |          /|        /|
+       /        3 -+--- 7  |         /_|______ / |
+      z         |  0 ---+- 4        |  |______|__|
+                |/      |/          | /       | /
+                1 ----- 5           |/________|/
+*/
+    return std::array<V3F,8>
+    {
+        mi,
+        { mi[0], mi[1], ma[2] },
+        { mi[0], ma[1], mi[2] },
+        { mi[0], ma[1], ma[2] },
+        { ma[0], mi[1], mi[2] },
+        { ma[0], mi[1], ma[2] },
+        { ma[0], ma[1], mi[2] },
+        ma
+    };
+}
+
+//==================================================================
+inline bool isClippedHomo( const auto &homoPos )
+{
+    return homoPos[0] < -homoPos[3]
+        || homoPos[0] >  homoPos[3]
+        || homoPos[1] < -homoPos[3]
+        || homoPos[1] >  homoPos[3]
+        || homoPos[2] <  0
+        || homoPos[2] >  homoPos[3];
+}
+
+//==================================================================
+class AtomObj
+{
+    const float         mDensity;
+
+    std::vector<V3F>    mPoses;
+    std::vector<V3I>    mCols;
+
+public:
+    AtomObj( float density ) : mDensity(density)
+    {
+    }
+
+    //
+    void ResetObj()
+    {
+        mPoses.clear();
+        mCols.clear();
+    }
+
+    //
+    void AddLine( const V3F &p1, const V3F &p2, const V3I &col )
+    {
+        const auto len = glm::length( p2 - p1 );
+
+        // minimum 1 point, max 200
+        const auto n = glm::clamp( (int)(len / mDensity), 1, 200 );
+
+        const auto oon_1 = 1.f / (float)(n-1);
+        for (int i=0; i < n; ++i)
+        {
+            const auto t = (float)i * oon_1;
+            const auto p = glm::mix( p1, p2, t );
+            mPoses.push_back( p );
+            mCols.push_back( col );
+        }
+    }
+
+    //
+    void AddWireCube( float siz, const V3I &col  )
+    {
+        const auto verts = makeCubeVerts( V3F{ -siz/2 }, V3F{ siz/2 } );
+        // bottom
+        AddLine( verts[0], verts[1], col );
+        AddLine( verts[1], verts[5], col );
+        AddLine( verts[5], verts[4], col );
+        AddLine( verts[4], verts[0], col );
+        // top
+        AddLine( verts[2], verts[3], col );
+        AddLine( verts[3], verts[7], col );
+        AddLine( verts[7], verts[6], col );
+        AddLine( verts[6], verts[2], col );
+        // vertical
+        AddLine( verts[3], verts[1], col );
+        AddLine( verts[7], verts[5], col );
+        AddLine( verts[6], verts[4], col );
+        AddLine( verts[2], verts[0], col );
+    }
+
+    //
+    void DrawAtom( auto *pRend, float x, float y, const V3I &col )
+    {
+        SDL_SetRenderDrawColor( pRend, col[0], col[1], col[2], 255 );
+
+        constexpr int W = 2;
+        constexpr int H = 2;
+
+        SDL_Rect rc;
+        rc.x = (int)(x - W/2.f);
+        rc.y = (int)(y - W/2.f);
+        rc.w = W;
+        rc.h = H;
+        SDL_RenderFillRect( pRend, &rc );
+    }
+
+    //
+    void DrawObj( auto *pRend, float deviceW, float deviceH, const MAT4 &proj_obj )
+    {
+        const auto n = mPoses.size();
+
+        struct OutVert
+        {
+            V3F pos;
+            V3I col;
+        };
+
+        std::vector<OutVert> outVerts;
+        outVerts.reserve( n );
+
+        for (size_t i=0; i < n; ++i)
+        {
+            // homogeneus coordinates (-w..w)
+            const auto homo = proj_obj * glm::vec4( mPoses[i], 1.f );
+
+            // clip in homogeneous space
+            if ( isClippedHomo( homo ) )
+                continue;
+
+            // convert to screen-space, meaning that anything visible is
+            // in the range -1..1 for x,y and 0..1 for z
+            const auto oow = 1.f / homo.w;
+            const auto screenX = homo.x * oow;
+            const auto screenY = homo.y * oow;
+            const auto screenZ = homo.z * oow;
+
+            // 0..deviceW (we flip vertically, from 3D standard to computer display standard)
+            const auto deviceX = deviceW * (screenX + 1) * 0.5f;
+            const auto deviceY = deviceH * (1 - screenY) * 0.5f;
+
+            // generate the output vertex in device-space for X and Y and screen-space for Z
+            // notice that Z is only used for soeting, so we could just use Z from homo space
+            outVerts.push_back({
+                    V3F{ deviceX, deviceY, screenZ },
+                    mCols[i] });
+        }
+
+        // sort with bigger Z first
+        std::sort( outVerts.begin(), outVerts.end(), []( const auto &l, const auto &r )
+        {
+            return l.pos.z > r.pos.z;
+        });
+
+        // finally render the points
+        for (const auto &v : outVerts)
+            DrawAtom( pRend, v.pos.x, v.pos.y, v.col );
+    }
+};
+
+//==================================================================
+static constexpr float CUBE_SIZ         = 1.f;      // 1 meter span
+static constexpr float CAMERA_DIST      = 2.f;      // 2 meters away
+static constexpr float CAMERA_FOV_DEG   = 70.f;     // field of view
+static constexpr float CAMERA_NEAR      = 0.01f;    // near plane (1 cm)
+static constexpr float CAMERA_FAR       = 100.f;    // far plane (100 m)
+
+//==================================================================
+inline float RAD2DEG( float rad )
+{
+    return glm::pi<float>() / 180.f * rad;
+}
+
+//==================================================================
+int main( int argc, char *argv[] )
+{
+    constexpr int  W = 640;
+    constexpr int  H = 480;
+
+    MinimalSDLApp app( "Demo3", W, H );
+
+    AtomObj obj( CUBE_SIZ / 20.f );
+
+    // begin the main/rendering loop
+    for (size_t frameCnt=0; ; ++frameCnt)
+    {
+        // begin the frame (or get out)
+        if ( !app.BeginFrame() )
+            break;
+
+        // get the renderer
+        auto *pRend = app.GetRenderer();
+
+        // clear the device
+        SDL_SetRenderDrawColor( pRend, 0, 0, 0, 0 );
+        SDL_RenderClear( pRend );
+
+        // create the verts
+        obj.ResetObj();
+        obj.AddWireCube( CUBE_SIZ, {0,255,0} );
+
+        // calculate the transforms
+        // object -> world transform
+        const auto world_obj = MAT4( 1.f );
+
+        // world -> camera transform
+        const auto camera_world = glm::translate(
+                                        MAT4(1.0f),
+                                        V3F(0.0f, 0.0f, -CAMERA_DIST) );
+
+        // camera -> projection
+        const auto proj_camera = glm::perspective(
+                                        RAD2DEG( CAMERA_FOV_DEG ),
+                                        (float)W / H,
+                                        CAMERA_NEAR,
+                                        CAMERA_FAR );
+
+        // final matrix, transforming obj -> projection
+        const auto proj_obj = proj_camera * camera_world * world_obj;
+
+        // draw the object
+        obj.DrawObj( pRend, W, H, proj_obj );
+
+        // end of the frame (will present)
+        app.EndFrame();
+
+        // reasonable frame rate, since there's no vsync
+        SDL_Delay( 10 );
+    }
+
+    return 0;
+}
+
